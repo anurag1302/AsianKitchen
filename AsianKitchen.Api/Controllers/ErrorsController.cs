11bc9e80806f4b73936bad2ab46a1fca0f6a444d
Anurag Sinha <anurag3487@gmail.com>
2024-02-29 13:14:14 +0530

    Extract exception from context and spread it to Problem result

--- AsianKitchen.Api/Controllers/ErrorsController.cs
+++ AsianKitchen.Api/Controllers/ErrorsController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AsianKitchen.Api.Controllers;
@@ -7,7 +10,9 @@ public class ErrorsController : ControllerBase
     [Route("/error")]
     public IActionResult Error()
     {
-        return Problem();
-    }
+        Exception exception = HttpContext.Features.Get<IExceptionHandlerFeature>().Error;
 
+        return Problem(title:exception.Message, 
+                       statusCode:(int)HttpStatusCode.InternalServerError);
+    }
 }
